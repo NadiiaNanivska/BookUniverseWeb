a2030b074d5b4d3fd7cefa469b42a4127c52fa3f
NadiiaNanivska <nanivskan14@gmail.com>
2024-04-03 18:06:12 +0300

    Implemented DTOs with validation and mapping

---
+++ BookUniverseProject/BookUniverse.Application/DTOs/UserDTOs/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BookUniverse.Application.DTOs.UserDTOs
+{
+    public class LoginDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ BookUniverseProject/BookUniverse.Application/Mapping/Books/BookProfile.cs
@@ -0,0 +1,23 @@
+﻿namespace BookUniverse.Application.Mapping.Books
+{
+    using AutoMapper;
+    using BookUniverse.Application.DTOs.BookDTOs;
+    using BookUniverse.Domain.Entities;
+
+    public class BookProfile : Profile
+    {
+        public BookProfile()
+        {
+            CreateMap<AddBookDto, Book>()
+                .ForMember(dest => dest.CategoryId, opt => opt.Ignore())
+                .AfterMap((src, dest, opt) =>
+                {
+                    if (opt.Items.TryGetValue("CategoryId", out var categoryId))
+                    {
+                        dest.CategoryId = (int)categoryId;
+                    }
+                })
+                .ReverseMap();
+        }
+    }
+}
