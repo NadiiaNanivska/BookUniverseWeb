f25febb911e9ad21c9af5a6af992ebbeb25bb4ce
KateYatsiuk <katerynayatsiuk@gmail.com>
2024-04-12 20:03:03 +0300

    Base init commit

---
+++ BookUniverseProject/BookUniverse.Application/MediatR/ResultVariations/NullResult.cs
@@ -0,0 +1,12 @@
+﻿using FluentResults;
+
+namespace BookUniverse.Application.MediatR.ResultVariations
+{
+    public class NullResult<T> : Result<T>
+    {
+        public NullResult()
+            : base()
+        {
+        }
+    }
+}
---
+++ BookUniverseProject/BookUniverseProject/Controllers/BaseApiController.cs
@@ -0,0 +1,37 @@
+﻿using BookUniverse.Application.MediatR.ResultVariations;
+using FluentResults;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookUniverseProject.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator? _mediator;
+
+        public BaseApiController()
+        {
+        }
+
+        protected IMediator Mediator => _mediator ??=
+            HttpContext.RequestServices.GetService<IMediator>()!;
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result.IsSuccess)
+            {
+                if (result is NullResult<T>)
+                {
+                    return Ok(result.Value);
+                }
+
+                return (result.Value is null) ?
+                    NotFound("Not Found") : Ok(result.Value);
+            }
+
+            return BadRequest(result.Reasons);
+        }
+    }
+}
